b0ec824fd57bdf2d443b33111b8782920d0ba7bf
Leon Klingele <git@leonklingele.de>
2025-02-02 00:07:57 +0100

    chore: v2

--- shell.nix
+++ shell.nix
@@ -1,25 +1,5 @@
-with import <nixpkgs> {}; let
-  unstable = pkgs.unstable or (import <unstable> {});
-in mkShell {
-  name = "tldwatch";
+{
+  system ? builtins.currentSystem,
+}:
 
-  nativeBuildInputs = [
-    gnumake
-
-    go
-    unstable.golangci-lint
-
-    air
-  ];
-
-  shellHook = ''
-    rootdir="$(realpath "$PWD")"
-    if [[ -n "$IN_LORRI_SHELL" ]]; then
-      rootdir="$(dirname "$IN_LORRI_SHELL")"
-    fi
-    export GOPATH="$rootdir/.gopath"
-    export GOBIN="$GOPATH/bin"
-    export GOCACHE="$GOPATH/.cache"
-    export PATH="$PATH:$GOBIN"
-  '';
-}
+(builtins.getFlake (toString ./.)).devShells.${system}.default
